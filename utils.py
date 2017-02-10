ca19e7c4597b263f72dc134d1e6390f64137fcdb
Brian-It <1183561811@qq.com>
2017-02-10 17:30:33 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,45 @@
 import os
 import sys
 
+if hasattr(sys, '_getframe'): currentframe = lambda: sys._getframe(3)
+_srcfile = os.path.normcase(currentframe.__code__.co_filename)
+
+
+def getCaller():
+    if _srcfile:
+        # IronPython doesn't track Python frames, so findCaller raises an
+        # exception on some versions of IronPython. We trap it here so that
+        # IronPython can use logging.
+        try:
+            fn, lno, func = findCaller()
+        except ValueError:
+            fn, lno, func = "(unknown file)", 0, "(unknown function)"
+    else:
+        fn, lno, func = "(unknown file)", 0, "(unknown function)"
+    return fn, lno, func
+
+
+def findCaller():
+    """
+    Find the stack frame of the caller so that we can note the source
+    file name, line number and function name.
+    """
+    f = currentframe()
+    # On some versions of IronPython, currentframe() returns None if
+    # IronPython isn't run with -X:Frames.
+    if f is not None:
+        f = f.f_back
+    rv = "(unknown file)", 0, "(unknown function)"
+    while hasattr(f, "f_code"):
+        co = f.f_code
+        filename = os.path.normcase(co.co_filename)
+        if filename == _srcfile:
+            f = f.f_back
+            continue
+        rv = (co.co_filename, f.f_lineno, co.co_name)
+        break
+    return rv
+
 
 def currentframe():
     """Return the frame object for the caller's stack frame."""
@@ -33,3 +72,4 @@ def print_caller(func):
         # print '######################################'
         func(*args, **kwargs)
     return wrapper
+
